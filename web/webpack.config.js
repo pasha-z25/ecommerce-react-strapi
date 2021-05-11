eb5db10bcc0b821d549b905ef03182c9e604197f
Pasha <pashazubak@gmail.com>
2021-05-11 22:25:32 +0300

    remove postcss-preset-env

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -12,8 +12,7 @@ const config = {
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'common.css',
-        }),
-        'postcss-preset-env',
+        })
     ],
     module: {
         rules: [
