655b97e9919612f8b89c2ed589be658857518656
Pasha <pashazubak@gmail.com>
2021-05-11 22:10:50 +0300

    added init files and configs

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <span>Hello world!</span>
+            <hr/>
+        </div>
+    )
+}
+
+export default App
---
+++ web/webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+    devtool: 'eval-source-map',
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: 'dist/'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'common.css',
+        }),
+        'postcss-preset-env',
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.module\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[local]__[sha1:hash:hex:7]'
+                            },
+                        }
+                    },
+                    'postcss-loader'
+                ],
+            },
+            {
+                test: /^((?!\.module).)*css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+            },
+            {
+                test: /\.module\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2,
+                            modules: {
+                                localIdentName: '[local]__[sha1:hash:hex:7]'
+                            },
+                        }
+                    },
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /^((?!\.module).)*s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+        ]
+    },
+}
+
+module.exports = config;
