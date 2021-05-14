1cddb38cd7b44ff5179fec8d544db2439f5a1b3d
admin <pashazubak@gmail.com>
2021-05-14 17:28:50 +0300

    minor changes

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
+// import { gql, useQuery } from '@apollo/client'
 
 const App = () => {
-    return (
-        <div>
-            <span>Hello world!</span>
-            <hr/>
-        </div>
-    )
+  console.log('2')
+  // const { loading, error, data } = useQuery(gql`
+  //   query Category {
+  //     category {
+  //       id
+  //       name
+  //       description
+  //       categories {
+  //         name
+  //       }
+  //     }
+  //   }
+  // `);
+  //
+  // if (loading) return 'Loading...';
+  // if (error) return `Error! ${error.message}`;
+  // console.log(data)
+
+  return (
+    <div>
+      <span>Hello world! 123 sgbsf g df</span>
+      <hr/>
+    </div>
+  )
 }
 
 export default App
