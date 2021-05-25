21b85655fedc56aa9d5921826e05350619342bd2
admin <pashazubak@gmail.com>
2021-05-25 17:45:28 +0300

    extend graphQL query, add router

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useQuery, gql } from '@apollo/client'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Header } from './scenes/Header'
+import { Home } from './scenes/HomePage'
+import { Products } from './scenes/Products'
 
 const App = () => {
-
   const { loading, error, data } = useQuery(gql`
     query Categories {
       categories {
@@ -13,18 +15,51 @@ const App = () => {
         updated_at
         published_at
       }
+      products {
+        id
+        created_at
+        Title
+        Slug
+        category {
+          id
+        }
+        Description
+        Price
+        InStock
+        OfficialPage
+        Photo {
+          id
+          name
+          url
+          caption
+          alternativeText
+        }
+      }
     }
-  `);
+  `)
 
-  if (data) {console.log(data)}
+  if (data) {
+    const { categories, products } = data
+    console.log(categories)
+    console.log(products)
+  }
+
 
   return (
     <div>
       {loading && <div>Loading...</div>}
       {error && <div>{`Error! ${error.message}`}</div>}
-      <span>Hello world!</span>
-      <Header />
-      <hr/>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/products'>
+            <Products />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }
