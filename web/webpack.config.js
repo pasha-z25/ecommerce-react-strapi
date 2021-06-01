57a960e075c36c501843ac4ddaf009ab8b37f28c
admin <pashazubak@gmail.com>
2021-06-01 10:06:38 +0300

    add resources rules

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -69,6 +69,14 @@ const config = {
         exclude: /node_modules/
       },
       {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource'
+      },
+      {
         test: /\.module\.css$/i,
         use: [
           MiniCssExtractPlugin.loader,
