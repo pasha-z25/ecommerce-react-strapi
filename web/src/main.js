07c1e73200ab7297184802a45bb36e172ff2db30
admin <pashazubak@gmail.com>
2021-06-02 11:08:44 +0300

    clear styles

--- web/src/main.js
+++ web/src/main.js
@@ -4,11 +4,6 @@ import { ApolloProvider } from '@apollo/client/react' // eslint-disable-line
 import App from './App'
 import client from './api/apollo'
 
-import './style.css'
-// import '~styles'
-import './styles/index.scss'
-// import './scenes/Header/styles.module.scss'
-
 ReactDom.render((
     <ApolloProvider client={client}>
       <App />
