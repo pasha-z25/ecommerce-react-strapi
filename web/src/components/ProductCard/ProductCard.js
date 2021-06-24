312684da5edecb9684da3cdbd3f9e46c01a3272b
admin <pashazubak@gmail.com>
2021-06-24 15:29:48 +0300

    remove some old code

--- web/src/components/ProductCard/ProductCard.js
+++ web/src/components/ProductCard/ProductCard.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-// import ReactMarkdown from 'react-markdown'
 import { ImageSlider } from './components/ImageSlider'
 
 const ProductCard = ({ product }) => {
-  console.log(product)
-
   return(
     <div className='relative'>
       <div className='relative slider-wrapper' style={{ paddingTop: '60%', marginTop: '15px', marginBottom: '15px'}}>
@@ -14,7 +11,6 @@ const ProductCard = ({ product }) => {
       </div>
       <h3>{ product.title }</h3>
       <p>{ product.introText }</p>
-      {/* <div><ReactMarkdown>{ product.description }</ReactMarkdown></div> */}
       <p>${ product.price }</p>
       <p>
         <Link to={`${process.env.PRODUCTS_SLUG}/${product.category.slug}`}>{product.category.title}</Link>
