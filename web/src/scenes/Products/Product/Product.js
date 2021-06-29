ec6b292288d3f3191c1901ba2c23fb729ef92d39
admin <pashazubak@gmail.com>
2021-06-29 18:12:31 +0300

    add additional functional

--- web/src/scenes/Products/Product/Product.js
+++ web/src/scenes/Products/Product/Product.js
@@ -1,15 +1,79 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { gql, useQuery } from '@apollo/client'
+import ReactMarkdown from 'react-markdown'
+import { ImageSlider } from './components/ImageSlider'
 
 const Product = () => {
-  const { slug } = useParams()
+  const { id } = useParams()
+  const { loading, error, data } = useQuery(gql`
+      query Product {
+          product(id:"${id}") {
+              id
+              title
+              introText
+              description
+              officialPage
+              category {
+                  id
+                  slug
+                  title
+                  description
+                  photo {
+                      id
+                      name
+                      alternativeText
+                      caption
+                      width
+                      height
+                      formats
+                      hash
+                      ext
+                      mime
+                  }
+              }
+              inStock
+              price
+              photo {
+                  id
+                  name
+                  alternativeText
+                  caption
+                  width
+                  height
+                  formats
+                  hash
+                  ext
+                  mime
+              }
+          }
+      }
+  `)
+  const product = data?.product
+
   return (
     <section>
-      <h2>One Product</h2>
-      <h3>{slug}</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-      <p>Accusamus amet assumenda, beatae consequatur cupiditate dicta dolorum esse est id labore neque nostrum.</p>
-      <p>Officia pariatur perspiciatis porro quam rem repellendus soluta?</p>
+      {loading && <div>Loading...</div>}
+      {error && <div>{`Error! ${error.message}`}</div>}
+      {product && (
+        <>
+          {/* product.photo.map((photo) => {
+            return (
+              <img key={photo.hash} src={`${process.env.API_HOST}${photo.formats.large?.url}`}
+                   alt={photo.alternativeText} title={photo.caption} />
+            )
+          }) */}
+          <div className='slider-wrapper relative' style={{ paddingTop: '60%'}}>
+              <ImageSlider images={product.photo} />
+          </div>
+          <h1>{product.title}</h1>
+          <p className='text-right'>
+              <Link to={`/${process.env.CATEGORIES_SLUG}/${product.category.slug}`}>{product.category.title}</Link>
+          </p>
+          <div><ReactMarkdown>{product.description}</ReactMarkdown></div>
+
+        </>
+      )}
     </section>
   )
 }
