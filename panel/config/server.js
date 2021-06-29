1d07750c81f0d2273ccecfb3656f5a3b29da6f5a
admin <pashazubak@gmail.com>
2021-06-29 15:49:09 +0300

    minor changes

--- panel/config/server.js
+++ panel/config/server.js
@@ -2,9 +2,8 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
-    url: '/',
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '17eca2b86209ffecf9c50c69a66c4ec8'),
-    },
-  },
-});
+      secret: env('ADMIN_JWT_SECRET', '17eca2b86209ffecf9c50c69a66c4ec8')
+    }
+  }
+})
