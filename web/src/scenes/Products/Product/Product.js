e1ae3fea588b444756acd084bd965d7d97ba0742
admin <pashazubak@gmail.com>
2021-07-01 17:53:53 +0300

    fix product view

--- web/src/scenes/Products/Product/Product.js
+++ web/src/scenes/Products/Product/Product.js
@@ -3,6 +3,9 @@ import { Link, useParams } from 'react-router-dom'
 import { gql, useQuery } from '@apollo/client'
 import ReactMarkdown from 'react-markdown'
 import { ImageSlider } from './components/ImageSlider'
+import { text } from '~modules/i18n'
+
+import styles from './styles.module.scss'
 
 const Product = () => {
   const { id } = useParams()
@@ -56,23 +59,24 @@ const Product = () => {
       {loading && <div>Loading...</div>}
       {error && <div>{`Error! ${error.message}`}</div>}
       {product && (
-        <>
-          {/* product.photo.map((photo) => {
-            return (
-              <img key={photo.hash} src={`${process.env.API_HOST}${photo.formats.large?.url}`}
-                   alt={photo.alternativeText} title={photo.caption} />
-            )
-          }) */}
-          <div className='slider-wrapper relative' style={{ paddingTop: '60%'}}>
+        <div className={`${styles.productWrapper} product-wrapper`}>
+          <div className={`${styles.sliderWrapper} slider-wrapper relative`}>
               <ImageSlider images={product.photo} />
           </div>
-          <h1>{product.title}</h1>
-          <p className='text-right'>
-              <Link to={`/${process.env.CATEGORIES_SLUG}/${product.category.slug}`}>{product.category.title}</Link>
-          </p>
-          <div><ReactMarkdown>{product.description}</ReactMarkdown></div>
-
-        </>
+          <div className={`${styles.productInfo} product-info`}>
+              <h1>{product.title}</h1>
+              <p>${product.price} ({product.inStock ? text('textInStock') : text('textUnderTheOrder')})</p>
+              <p>
+                  <Link to={`/${process.env.CATEGORIES_SLUG}/${product.category.slug}`}>{product.category.title}</Link>
+              </p>
+              <p>
+                  <a href={product.officialPage} target='_blank' rel='noopener noreferrer'>{ text('textOfficialPage') }</a>
+              </p>
+          </div>
+          <div className={`${styles.productDescription} product-description`}>
+              <ReactMarkdown>{product.description}</ReactMarkdown>
+          </div>
+        </div>
       )}
     </section>
   )
