9bb6adef5d7352fcc60dc6c157f05749f11f80bf
admin <pashazubak@gmail.com>
2021-07-01 17:52:31 +0300

    add Cart and Languages components

--- web/src/scenes/Header/Header.js
+++ web/src/scenes/Header/Header.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { Menu } from './components/Menu'
+import { Cart } from './components/Cart'
+import { SelectLanguages } from './components/SelectLanguages'
 import styles from './styles.module.scss'
 import Logo from '~assets/icons/Webpack_and_React.png'
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className='container mx-auto flex flex-wrap items-center justify-between'>
+      <div className='container mx-auto flex items-center justify-between'>
         <div className={styles.logo}>
           <img src={Logo} alt='Webpack_and_React' />
         </div>
-        <nav>
+        <nav className='flex-1 px-4'>
           <Menu />
         </nav>
+        <Cart />
+        <SelectLanguages />
       </div>
     </header>
   )
